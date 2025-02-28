3cc71f4eef6079fd8049d7fc64ce3aa436cc2c49
IanuReeves <66649143+IanuReeves@users.noreply.github.com>
2025-02-27 17:39:57 -0800

    Fix my fixing of someones mistake mb
    I did a stupid

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.	
 func _on_start_button_pressed() -> void:
 	print("Game Started. Loading World...")
-	get_tree().change_scene_to_file("res://scenes/ui/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 
