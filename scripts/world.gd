52fe574669aaac8acf9ca7caadc86a8b834ec2db
Ian Andree <iangustav@proton.me>
2025-03-06 15:35:50 -0800

    Added pause menu UI and Script
    may add shaders as wel

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,3 +1,18 @@
 extends Node2D
 
-@onready var ui = %ui
+@onready var pause_menu: Control = $Player/UI/pause_menu
+var paused = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+		
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	paused = !paused
