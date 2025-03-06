028ad2f8f1e12a69e94a427d74f914c1801431b1
_Diji <Digiman000@users.noreply.github.com>
2025-03-06 15:32:29 -0800

    almost made enemy shots lead

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -4,7 +4,7 @@ class_name Weapon
 @export var bullet:PackedScene
 @export var target_layer:int
 #fire bullet out of weapon
-func fire(speedmod:Vector2,damage:float,bullet_speed:float,bullet_amount:int = 1,bullet_spread:float = 0):
+func fire(target_layer:int,speedmod:Vector2,damage:float,bullet_speed:float,bullet_amount:int = 1,bullet_spread:float = 0):
 	# for number of bullets
 	for i in range(0,bullet_amount):
 		# create a bullet and modify it's stats based on input
